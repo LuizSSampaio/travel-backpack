1b4c7994167aecf8f765bca886eff6b928ca63ae
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-04-20 17:53:17 -0300

    LocalStorage and some refactors

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 const form = document.querySelector("#novoItem");
 const list = document.querySelector("#list");
+const items =
+    localStorage.getItem("items") != undefined
+        ? /*localStorage.getItem("items")*/ []
+        : [];
+
+console.log(localStorage.getItem("items"));
 
 form.addEventListener("submit", (eventSubmit) => {
     eventSubmit.preventDefault();
 
-    insertElement(
-        createListItem(
-            eventSubmit.target["nome"].value, 
-            eventSubmit.target["quantidade"].value
-        )
-    );
+    const nameElement = eventSubmit.target["nome"];
+    const quantityElement = eventSubmit.target["quantidade"];
+
+    insertElement(createListItem(nameElement.value, quantityElement.value));
+
+    nameElement.value = "";
+    quantityElement.value = 1;
 });
 
 function createListItem(name, quantity) {
     const itemQuantity = document.createElement("strong");
     itemQuantity.innerHTML = quantity;
 
-    const listItem = document.createElement("li")
+    const listItem = document.createElement("li");
     listItem.appendChild(itemQuantity);
     listItem.innerHTML += name;
     listItem.classList.add("item");
+
+    items.push(JSON.stringify({ name, quantity }));
+
+    localStorage.setItem("items", items);
 
     return listItem;
 }
